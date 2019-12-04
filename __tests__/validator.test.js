2c60367ad622db2c8af57e0e3f39f2d7a1fc66d0
Ian Andrewson <ianandrewson@gmail.com>
2019-12-03 17:44:18 -0800

    tests improved

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,19 +1,63 @@
-import Validator from '../lib/validator.js';
+const { Validator } = require('../lib/validator.js');
+
+const dog = {
+    name: 'Spot',
+    age: 5,
+    weight: '20 lbs'
+};
+
+//required and field there and right type
+const nameValidator = new Validator('name', {
+    type: String,
+    required: true
+});
+
+//required and field missing
+const colorValidator = new Validator('color', {
+    type: String,
+    required: true
+});
+
+//required and field there and wrong type
+const weightValidator = new Validator('weight', {
+    type: Number,
+    required: true
+});
+
+//not required and field there and right type
+const ageValidator = new Validator('age', {
+    type: Number,
+    required: false
+});
+
+//not required and field missing
+const rabidValidator = new Validator('rabid', {
+    type: String,
+    required: false
+});
+
+//not required and field there and wrong type
+const goodBoiValidator = new Validator('goodBoi', {
+    type: Boolean,
+    required: false
+})
 
 describe('Validator class tests', () => {
     it('should take an object and return a fields value', () => {
-        expect();
+        expect(nameValidator.validate(dog)).toEqual('Spot');
+        expect(ageValidator.validate(dog)).toEqual('5');
+        expect(weightValidator.validate(dog)).toEqual('20 lbs');
     });
     it('should take an object and throw an error if field does not exist', () => {
-        expect();
+        expect(() => {colorValidator(dog);}).toThrowError();
     });
     it('should handle all permutations of requirement, field presence, and type', () => {
-        expect(validate()).toThrowError();
-        expect(validate()).toThrowError();
-        expect(validate()).toBeTruthy();
-        expect(validate()).toBeTruthy();
-        expect(validate()).toThrowError();
-        expect(validate()).toBeTruthy();
+        expect(colorValidator.validate(dog)).toThrowError();
+        expect(weightValidator.validate(dog)).toThrowError();
+        expect(nameValidator.validate(dog)).toBeTruthy();
+        expect(rabidValidator.validate(dog)).toBeTruthy();
+        expect(goodBoiValidator.validate(dog)).toThrowError();
+        expect(ageValidator.validate(dog)).toBeTruthy();
     });
 });
 
